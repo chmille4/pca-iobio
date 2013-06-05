67a05ca820ffbfa78479043e4a83ca4178bc1ae9
Chase Miller <chmille4@gmail.com>
2013-06-05 15:10:34 -0400

    Add iobio as submodule
    add submodule
    update start and stop servers script to work with name change

--- startServers.sh
+++ startServers.sh
@@ -5,13 +5,13 @@ forever start pageServer.js --port 7070
 
 # start server that reads cached gds files
 # gds files are what snpRelate creates after processing the VCF
-forever start minion/services/gdsServer.js --port 8000
+forever start iobio/services/gdsServer.js --port 8000
 
 # start pca server
-forever start minion/services/snpRelateServer.js --port 8010
+forever start iobio/services/snpRelateServer.js --port 8010
 
 # start vcflib server
-forever start minion/services/vcflibServer.js --port 7080
+forever start iobio/services/vcflibServer.js --port 7080
 
 # start tabix server to download vcf slices
-forever start minion/services/tabixServer.js --port 7090
+forever start iobio/services/tabixServer.js --port 7090
--- stopServers.sh
+++ stopServers.sh
@@ -2,7 +2,7 @@
 
 # stop servers
 forever stop pageServer.js
-forever stop minion/services/gdsServer.js
-forever stop minion/services/snpRelateServer.js
-forever stop minion/services/vcflibServer.js
-forever stop minion/services/tabixServer.js
+forever stop iobio/services/gdsServer.js
+forever stop iobio/services/snpRelateServer.js
+forever stop iobio/services/vcflibServer.js
+forever stop iobio/services/tabixServer.js
