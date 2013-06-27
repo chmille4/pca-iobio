d3b8c3b2d2403442a2ebb94c8096c98a06ed7d5c
Chase Miller <chmille4@gmail.com>
2013-06-27 13:10:55 -0400

    remove iobio as submodule

--- stopServers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# stop servers
-forever stop pageServer.js
-forever stop iobio/services/gdsServer.js
-forever stop iobio/services/snpRelateServer.js
-forever stop iobio/services/vcflibServer.js
-forever stop iobio/services/tabixServer.js
