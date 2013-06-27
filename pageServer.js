f2b9c0745b11984848826b59a369b511a7f0cf37
Chase Miller <chmille4@gmail.com>
2013-06-27 13:15:20 -0400

    moved all resources from public directory to main directory and updated index and server accordingly

--- pageServer.js
+++ pageServer.js
@@ -19,7 +19,7 @@ var io = require('socket.io').listen(server);
 server.listen(port);
 
 // setup server
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname ));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
